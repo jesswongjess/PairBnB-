22e7057ba96a75bf929e082bb1959a625bb3ccc6
JESS WONG <jesswongjess@gmail.com>
2017-05-01 22:42:31 +0800

    nstalled Figaro Gem

---
+++ db/migrate/20170426102153_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :nationality 
+      t.timestamps null: false
+      t.string :email, null: false
+      t.string :encrypted_password, limit: 128, null: false
+      t.string :confirmation_token, limit: 128
+      t.string :remember_token, limit: 128, null: false
+
+    end
+
+    add_index :users, :email
+    add_index :users, :remember_token
+  end
+end
