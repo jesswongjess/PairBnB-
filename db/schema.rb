36044367bafc81fa5dd851b172d1333d183886ff
JESS WONG <jesswongjess@gmail.com>
2017-05-02 18:52:54 +0800

    create listing, tag, listing tags and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502033441) do
+ActiveRecord::Schema.define(version: 20170502082559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,37 @@ ActiveRecord::Schema.define(version: 20170502033441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_authentications_on_user_id", using: :btree
+  end
+
+  create_table "listing_tags", force: :cascade do |t|
+    t.integer  "listing_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_listing_tags_on_listing_id", using: :btree
+    t.index ["tag_id"], name: "index_listing_tags_on_tag_id", using: :btree
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "property_type"
+    t.integer  "room_type"
+    t.integer  "guest"
+    t.integer  "bedroom"
+    t.integer  "bathroom"
+    t.integer  "price"
+    t.string   "address"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.integer  "tag_content", default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +72,6 @@ ActiveRecord::Schema.define(version: 20170502033441) do
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "listing_tags", "listings"
+  add_foreign_key "listing_tags", "tags"
 end
